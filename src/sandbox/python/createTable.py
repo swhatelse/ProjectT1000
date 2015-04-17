1196670899f8afab84adc418dae6d4b3384682f9
ThibaudB <Thibaud.Buchs@gmail.com>
2015-04-17 11:38:31 +0200

    fixes

--- src/sandbox/python/createTable.py
+++ src/sandbox/python/createTable.py
@@ -15,10 +15,10 @@ def createPython(reds, yellows, emptys):
 	for e in allInOne:
 		if (e.x==reds[0].x &&  e.y==reds[0].y):
 			plateau.plateau[x][y] = Plateau.J[1];
-			
+			reds.remove(0);
 		else if (e.x==yellows[0].x &&  e.y==yellows[0].y):
 			plateau.plateau[x][y] = Plateau.J[2];
-			
+			yellows.remove(0);
 		x += 1;
 		if (x == plateau.NB_LIGNE):
 			y += 1;
