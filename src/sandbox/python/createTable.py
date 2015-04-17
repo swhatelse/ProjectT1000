98eb6c6167af4b58fac4267d9e85c856dd488d25
ThibaudB <Thibaud.Buchs@gmail.com>
2015-04-17 11:29:10 +0200

    createTable.py

---
+++ src/sandbox/python/createTable.py
@@ -0,0 +1,25 @@
+import Plateau
+
+def createPython(reds, yellows, emptys):
+	reds.sort();
+	yellows.sort();
+	allInOne = reds+yellows+emptys;
+    allInOne.sort();
+    
+    if len(allInOne) != 42 :
+		raise None;
+	
+	x = 0;
+	y = 0;
+	plateau = Plateau.Plateau();
+	for e in allInOne:
+		if (e.x==reds[0].x &&  e.y==reds[0].y):
+			plateau.plateau[x][y] = Plateau.J[1];
+			
+		else if (e.x==yellows[0].x &&  e.y==yellows[0].y):
+			plateau.plateau[x][y] = Plateau.J[2];
+			
+		x += 1;
+		if (x == plateau.NB_LIGNE):
+			y += 1;
+	return plateau;
