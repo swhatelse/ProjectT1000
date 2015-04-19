0379874638eca1cd4cdb055add8575701e702f35
swhatelse <stevenwhatelse@hotmail.fr>
2015-04-19 12:37:07 +0000

    detection implem final

---
+++ src/senses/Detector.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python2
+
+# Standard imports
+import cv2
+import numpy as np;
+import time
+import tracker as tr
+import Color
+
+def nothing(x):
+    pass
+
+class Detector(object):
+
+    def __init__(self, debug = False):
+        self.params = cv2.SimpleBlobDetector_Params()
+        self.params.minArea = 50
+        self.detector = cv2.SimpleBlobDetector(self.params)
+        self.tracker = tr.Tracker()
+        self.DEBUG = debug
+        
+    def detectColor(self, img, color):
+        hsv = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+        blur = cv2.medianBlur(hsv,5)
+        binary = cv2.inRange(blur, color.lowHSV, color.highHSV)
+        return binary
+
+    # Return sets of keypoint for plateau and yellow and red pions
+    # it also returns the image in debug mode on which the algo as
+    # worked on
+    def findObjects(self, img, color = None):        
+        blue = Color.Color(104,75,46,151,255,255)
+        yellow = Color.Color(20,182,183,40,255,255)
+        red1 = Color.Color(0,168,100,6,255,255)
+        red2 = Color.Color(123,168,97,179,255,255)
+
+        # isolate plateau
+        if self.DEBUG:
+            self.tracker = tr.Tracker(True)
+        else:
+            self.tracker = tr.Tracker()
+        img, binary = self.tracker.detect(img,blue)
+
+        if color:
+            blBin = self.detectColor(img,color);
+        else:
+            blBin = self.detectColor(img,blue);
+        ylBin = self.detectColor(img,yellow);
+        rdBin1 = self.detectColor(img,red1);
+        rdBin2 = self.detectColor(img,red2);
+
+        rdBin = cv2.bitwise_or(rdBin1, rdBin2);
+
+        # for blue, we don't need to invert
+        blKp = self.detector.detect(blBin);
+        ylKp = self.detector.detect(~ylBin);
+        rdKp = self.detector.detect(~rdBin);
+
+        if self.DEBUG:
+            return blKp, ylKp, rdKp, img
+        else:
+            return blKp, ylKp, rdKp
+    
+if __name__ == '__main__':
+    blob = Blob()
+    blob.run()
---
+++ src/senses/TestDetector.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python2
+
+import cv2
+import Color
+import Detector as bl
+import numpy as np
+
+def nothing(x):
+    pass
+
+class TestDetector(object):
+    def __init__(self):
+        cv2.namedWindow('track')
+
+        cv2.createTrackbar('lowH','track',0,179,nothing)
+        cv2.createTrackbar('lowS','track',0,255,nothing)
+        cv2.createTrackbar('lowV','track',0,255,nothing)
+        cv2.createTrackbar('highH','track',179,179,nothing)
+        cv2.createTrackbar('highS','track',255,255,nothing)
+        cv2.createTrackbar('highV','track',255,255,nothing)
+
+    def calibrate(self):
+        lowH = cv2.getTrackbarPos('lowH','track')
+        lowS = cv2.getTrackbarPos('lowS','track')
+        lowV = cv2.getTrackbarPos('lowV','track')
+        highH = cv2.getTrackbarPos('highH','track')
+        highS = cv2.getTrackbarPos('highS','track')
+        highV = cv2.getTrackbarPos('highV','track')
+        color = Color.Color(lowH,lowS,lowV,highH,highS,highV)
+        return color
+
+    def detectColor(self, img, color):
+        hsv = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+        hsv = cv2.medianBlur(hsv,5)
+        binary = cv2.inRange(hsv, color.lowHSV, color.highHSV)
+        cv2.imshow('bin', binary)
+        return binary
+
+    def run(self):
+        detector = bl.Detector(True)
+         
+        while True:
+            frame = cv2.imread('../Images/P4_Lointain.jpg',1)
+            # frame = cv2.imread('../Images/test_P4_1.jpg',1)
+            # frame = cv2.imread('../cam/img/280px-Puissance4_01.svg.png',1)
+
+            # color = self.calibrate()
+            # binary = self.detectColor(frame, color)
+            # cv2.imshow('image', frame)
+            
+            blKp, ylKp, rdKp, img  = detector.findObjects(frame)
+            img = cv2.drawKeypoints(img, blKp, np.array([]), (255,0,0), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+            img = cv2.drawKeypoints(img, ylKp, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+            img = cv2.drawKeypoints(img, rdKp, np.array([]), (255,255,0), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+            cv2.imshow('img', img)
+            print(len(rdKp))
+            
+            if cv2.waitKey(30) ==  ord('q'):
+                break
+            
+            
+if __name__ == '__main__':
+    app = TestDetector()
+    app.run()
